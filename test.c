50f101f1a313d74469329ce88ee6213aba627afb
unknown <you@example.com>
2025-02-18 15:16:14 +0530

    Calculation Of Two Number

--- test.c
+++ test.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
-void main()
-{
-	printf(How are you Sir)
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
 }
